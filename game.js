2a95b063f614e2b9a95102e2b2f1518345c976c9
Mim Ahmed <MimAhmed@users.noreply.github.com>
2023-08-22 15:04:25 +0600

    Add source files

---
+++ game.js
@@ -0,0 +1,61 @@
+let timer = 100; // use for  game timer
+let score = 0;
+let hitrn = 0;
+
+function randomNumGenerator(){
+     hitrn = Math.floor(Math.random()*20);
+     return hitrn;
+}
+
+function increseScore(){
+    score += 10;
+    document.querySelector("#increseval").textContent = score;
+    
+}
+
+function makeBubble(){
+let bubble = "";
+
+for(let i = 1; i<150;i++){
+    // let randomNumGenerator = Math.floor(Math.random()*10);
+    randomNumGenerator();
+    bubble += `<div class="bubble">${hitrn}</div>`;
+}
+
+document.querySelector("#pbtm").innerHTML = bubble;
+}
+
+function runTimer(){
+  let timeTracker = setInterval(function(){
+        if(timer>0){
+            timer--;
+        document.querySelector("#timeVal").textContent = timer;
+        }
+        else{
+           clearInterval(timeTracker);
+           document.querySelector("#timeVal").textContent = "--";
+           document.querySelector("#pbtm").innerHTML = `<h1 id="cen"> Time Over </h1>`;
+        }
+    },1000)
+}
+
+function getNewHit(){
+    // hitrn = Math.floor(Math.random()*20);
+    randomNumGenerator();
+    document.querySelector("#newHit").textContent = hitrn;
+}
+
+document.querySelector('#pbtm').addEventListener("click",function(d){
+    // alert("run");
+    let clickEventNumber = Number(d.target.textContent);
+    if(clickEventNumber === hitrn){
+        increseScore();
+        makeBubble();
+        getNewHit();
+    }
+});
+
+// Function Calls
+makeBubble();
+runTimer();
+getNewHit();
